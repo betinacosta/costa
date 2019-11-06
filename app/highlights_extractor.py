e08b56893ad45fa939fb84f8816b2eba737b8711
Betina Costa <bmcosta13@gmail.com>
2019-11-06 20:05:58 -0300

    [WIP] test google client connection

--- app/highlights_extractor.py
+++ app/highlights_extractor.py
@@ -1,15 +1,21 @@
-from httplib2 import Http
-from googleapiclient.discovery import build
-
-from app.gmail_client import GmailClient
+from gmail_client import GmailClient
 
 
-class HighlightsExtractor:
+def main():
+    client = GmailClient()
 
-    def get_service(self):
-        gmail_client = GmailClient()
-        credentials = gmail_client.get_credentials()
-        service = build("gmail", "v1", http=credentials.authorize(Http()))
+    service = client.authenticate()
 
-        return service
+    # message_list = client.get_messages_matching_query(service, "from:no-reply@amazon.com has:attachment")
 
+    # message = client.get_message_details(service, "16e395b3b1ff9e44")
+
+    client.download_attachment(service, "16e395b3b1ff9e44", "ANGjdJ-zj8NeFS5WDxU4gQ54xHPMZxpScOjRb4pw2_7KM--Jb_7e2rWRutqma1VQW-lCeKD0hs5XPehNOPYT3V4u9dL0cj26_WObMOivb08ID6Z1UemxA8HVWiw5ts4x-YELH6IWgs848Pk7G_60uyjiAcs_2yE5NeOhV22_--JTJ3TyMiyRL3KikmbtrHHkixLsA03IVfgCCdKMVaPHEd6-R00IQo3-AE_vyDsPkPnyLi2AWPQ33YLHTYniC8eHfH2ahPyYzCjelRhEZ9qEvdyI5W2b7GOdIcFXoNiMLr5ThXajOelosNrUMhfsJcD_VHrlbArd_R-l6I13tdIcqpUqJmOETjqRiGJr6Pr-rCN6UiejNLoP8xnHagRbvAc")
+
+
+
+    # print(message)
+
+
+if __name__ == '__main__':
+    main()
